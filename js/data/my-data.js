7748c2f79c3140c9c2fcf203db98b97cb3be50a7
Rashid Abbasi <rashidabbasi17@gmail.com>
2026-05-18 02:39:02 +0500

    Update data

--- js/data/my-data.js
+++ js/data/my-data.js
@@ -3,7 +3,10 @@ const myData = {
   title: "Senior Software Engineer",
   tagline: "Full-Stack Software Engineer",
   description: "Expert in .NET Core, React, and Banking-Grade Financial Systems. Delivering scalable architecture for mission-critical applications.",
-  aboutMe: "Senior Software Engineer with over 5 years of experience in the fintech and product sectors. Specialized in building high-concurrency systems using .NET Core, Laravel, and React. I focus on logic-driven development and system-to-system integrations.",
+  aboutMe: `I’m a seasoned Senior Software Engineer with over six years of diverse experience across the banking sector and various project/product-based companies. My technical acumen spans a wide array of technologies including PHP Laravel, .NET, Vue.js, React.js, Node.js, Sockets, MySQL, and Oracle, amongst others. Recently, I’ve expanded my expertise to embrace the transformative potential of AI, utilizing tools like ChatGPT/GPT Pilot to enhance productivity and decision-making processes.
+    My journey in tech is driven by a profound curiosity and a continuous desire to learn and adapt. This mindset has been crucial in keeping pace with the rapid advancements in our field, particularly in the realms of artificial intelligence and machine learning.
+    In my role, I not only focus on code quality and system scalability but also emphasize effective communication. I have a proven track record of translating complex technical concepts into clear, concise language that stakeholders at all levels can understand. My leadership is defined by my ability to mentor and inspire teams, guiding them through complex projects with a focus on timely delivery and innovation.
+    I am on the lookout for opportunities where I can leverage my deep industry knowledge and my enthusiasm for AI to drive success and innovation. Let’s connect and explore how we can push the boundaries of what’s possible in technology together!`,
   footerAboutMe: "Senior software developer building finance-grade web apps, APIs, and automation systems. Always focused on clean architecture and strong delivery.",
   image: "uploads/pp.png",
   email: "rashidabbasi17@gmail.com",
@@ -13,9 +16,13 @@ const myData = {
     {
       degree: "BS in Computer Science",
       institution: "FAST NUCES - Karachi",
-      year: "2021"
+      year: "2019"
     },
-    
+    {
+      degree: "Intermediate in Pre-Engineering",
+      institution: "Superior College",
+      year: "2015"
+    },
   ],
   social: {
     facebook: "https://www.facebook.com/rashidabbasi14",
