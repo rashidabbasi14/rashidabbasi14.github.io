5a89017224e240824c14202ae4dd7301a434f5ea
Rashid Abbasi <rashidabbasi17@gmail.com>
2026-05-18 08:27:55 +0500

    Update

--- js/common/navbar.js
+++ js/common/navbar.js
@@ -3,17 +3,17 @@ function renderNavbar() {
   const navbarHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top shadow-sm">
         <div class="container">
-            <a class="navbar-brand fw-bold" id="navbar-brand" href="index.html">Rashid Ahmed Abbasi</a>
+            <a class="navbar-brand fw-bold" id="navbar-brand" href="/">Rashid Ahmed Abbasi</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="mainNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="index.html#home">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.html#employment">Employment</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/#home">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/#employment">Employment</a></li>
                     <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.html#certifications">Certifications</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.html#contact">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/#certifications">Certifications</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
